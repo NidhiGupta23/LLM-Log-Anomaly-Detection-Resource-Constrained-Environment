d7c267bef3a7f6ffd349ba5097949b62e838e3bf
NidhiGupta23 <guptanidhicontact@gmail.com>
2026-05-21 01:33:57 +0000

    Adding BGL few shot DeepSeek 8GB CPU only

---
+++ Deepseek_R1_Distill_Qwen_1.5B/few_shot/deepseek_bgl.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# This script runs Python files sequentially
+
+# Exit immediately if a command fails
+set -e
+
+# Run each Python file in sequence
+
+#python few_shot_bgl_2ab2nmt10.py
+#python few_shot_bgl_3ab1nmt10.py
+#python few_shot_bgl_1ab3nmt10.py
+#python few_shot_bgl_4normt50.py
+#python few_shot_bgl_4abnmt50.py
+#python few_shot_bgl_3ab1nmt50.py
+#python few_shot_bgl_1ab3nmt50.py
+#python few_shot_bgl_4normt75.py
+#python few_shot_bgl_4abnmt75.py
+#python few_shot_bgl_2ab2nmt75.py
+#python few_shot_bgl_3ab1nmt75.py 
+python few_shot_bgl_1ab3nmt75.py
+python few_shot_bgl_4normt100.py
+python few_shot_bgl_4abnmt100.py
+python few_shot_bgl_2ab2nmt100.py
+python few_shot_bgl_3ab1nmt100.py
+python few_shot_bgl_1ab3nmt100.py 
+python few_shot_bgl_4normt512.py
+python few_shot_bgl_4abnmt512.py
+python few_shot_bgl_2ab2nmt512.py 
+python few_shot_bgl_3ab1nmt512.py
+python few_shot_bgl_1ab3nmt512.py
+
+echo "All scripts executed successfully."
