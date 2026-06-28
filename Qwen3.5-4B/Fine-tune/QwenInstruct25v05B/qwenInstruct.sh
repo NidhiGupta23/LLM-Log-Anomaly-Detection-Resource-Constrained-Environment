032684853034ef7de16b23db2deff0337db9e2db
NidhiGupta23 <guptanidhicontact@gmail.com>
2026-06-28 14:18:38 +0000

    Fine tuned Qwen Instruct and tested with 3 variatios

---
+++ Qwen3.5-4B/Fine-tune/QwenInstruct25v05B/qwenInstruct.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# This script runs Python files sequentially
+
+# Exit immediately if a command fails
+set -e
+
+
+python3 EvalQwnInstruct.py  --gguf   /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b.f16.gguf   --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstruct16gguf500logs_modeFull_v1.json --mode full
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b.f16.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstruct16gguf500logs_modeExtended_v1.json --mode extended
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b.f16.gguf   --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstruct16gguf500logs_modeMinimal_v1.json --mode minimal
+python3 EvalQwn2columnInstruct.py   --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b.f16.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstruct16gguf500logs_modeOriginal_v1.json
+
+
+
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ4_K_M500logs_modeFull_v1.json --mode full
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ4500logs_modeExtended_v1.json --mode extended                                                                               
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf   --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ4500logs_modeMinimal_v1.json --mode minimal
+python3 EvalQwn2columnInstruct.py   --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ4500logs_modeOriginal_v1.json
+
+
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q8_0.gguf --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ8500logs_modeFull_v2.json --mode full
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q8_0.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ8500logs_modeExtended_v1.json --mode extended
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf   --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ8500logs_modeMinimal_v1.json --mode minimal
+python3 EvalQwn2columnInstruct.py   --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q8_0.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ8500logs_modeOriginal_v1.json
+
+
+
+python3 EvalQwnInstruct.py  --gguf  /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b.f16.gguf   --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstruct16gguf500logs_modeFull_v2.json --mode full
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b.f16.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstruct16gguf500logs_modeExtended_v2.json --mode extended
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b.f16.gguf   --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstruct16gguf500logs_modeMinimal_v2.json --mode minimal
+python3 EvalQwn2columnInstruct.py   --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b.f16.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstruct16gguf500logs_modeOriginal_v2.json
+
+
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ4_K_M500logs_modeFull_v2.json --mode full
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ4500logs_modeExtended_v2.json --mode extended
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf   --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ4500logs_modeMinimal_v2.json --mode minimal
+python3 EvalQwn2columnInstruct.py   --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ4500logs_modeOriginal_v2.json
+
+
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q8_0.gguf --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ8500logs_modeFull_v2.json --mode full
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q8_0.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ8500logs_modeExtended_v2.json --mode extended
+python3 EvalQwnInstruct.py  --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q4_K_M.gguf   --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ8500logs_modeMinimal_v2.json --mode minimal
+python3 EvalQwn2columnInstruct.py   --gguf /root/.cache/huggingface/hub/models--NidhiGupta23--QwenInstruct25v05B/snapshots/132d2f74c8b824e9242a2b265c16e5ec49e3c275/bgl_1.5b_Q8_0.gguf  --test ../../../Deepseek_R1_Distill_Qwen_1.5B/fine_tuning/Test_500_no_label_sorted.log --output QwenInstructQ8500logs_modeOriginal_v2.json
